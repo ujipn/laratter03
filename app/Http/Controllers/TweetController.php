b31bf9c2ad0a63a79257b8985fb8fc3a47e65bff
ujipn <hiroaki.uji8@gmail.com>
2024-02-13 01:03:58 +0900

    test

---
+++ app/Http/Controllers/TweetController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tweet;
+use Illuminate\Http\Request;
+
+class TweetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $tweets = Tweet::with('user')->latest()->get();
+        return view('tweets.index', compact('tweets'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        return view('tweets.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'tweet' => 'required|max:255',
+            'area' => 'required', //areaを追加してみました
+          ]);
+      
+          $request->user()->tweets()->create($request->only('tweet','area'));
+      
+          return redirect()->route('tweets.index');
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tweet $tweet)
+    {
+        //
+        return view('tweets.show', compact('tweet'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tweet $tweet)
+    {
+        //
+        return view('tweets.edit', compact('tweet'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tweet $tweet)
+    {
+        //
+        $request->validate([
+            'tweet' => 'required|max:255',
+            'area' => 'required',//areaを追加
+          ]);
+      
+          $tweet->update($request->only('tweet','area'));
+      
+          return redirect()->route('tweets.show', $tweet);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tweet $tweet)
+    {
+        //
+        $tweet->delete();
+
+    return redirect()->route('tweets.index');
+    }
+}
---
+++ app/Models/Tweet.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tweet extends Model
+{
+    use HasFactory;
+    protected $fillable = ['tweet','area'];
+
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
+
+  public function liked()
+  {
+      return $this->belongsToMany(User::class)->withTimestamps();
+  }
+}
